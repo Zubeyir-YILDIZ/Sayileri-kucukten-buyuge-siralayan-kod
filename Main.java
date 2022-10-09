0860ea7b90e2d923616d8e3902633a9b2776b388
Zubeyir-YILDIZ <115133461+Zubeyir-YILDIZ@users.noreply.github.com>
2022-10-09 18:32:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,34 +7,41 @@ public class Main
         Scanner input=new Scanner(System.in);
 
         System.out.print("1.Sayıyı giriniz: ");    sayi1=input.nextInt();
-        System.out.println("2.Sayıyı giriniz: ");  sayi2=input.nextInt();
-        System.out.println("3.Sayıyı giriniz: ");  sayi3=input.nextInt();
+        System.out.print("2.Sayıyı giriniz: ");  sayi2=input.nextInt();
+        System.out.print("3.Sayıyı giriniz: ");  sayi3=input.nextInt();
 
-        if(sayi3>sayi2)
+        if(sayi1!=sayi2&&sayi1!=sayi3&&sayi2!=sayi3)
         {
-            if(sayi2>sayi1)
+            if(sayi3>sayi2)
             {
-                System.out.println(sayi1+"<"+sayi2+"<"+sayi3);
-            }
-            else
+                if(sayi2>sayi1)
+                {
+                    System.out.println(sayi1+"<"+sayi2+"<"+sayi3);
+                }
+                else
                 if(sayi1<sayi3)
                     System.out.println(sayi2+"<"+sayi1+"<"+sayi3);
                 else
                     System.out.println(sayi2+"<"+sayi3+"<"+sayi1);
-        }
-        else
-        {
-            if(sayi3>sayi1)
-                System.out.println(sayi1+"<"+sayi3+"<"+sayi2);
+            }
             else
             {
-                if(sayi1<sayi2)
-                    System.out.println(sayi3+"<"+sayi1+"<"+sayi2);
+                if(sayi3>sayi1)
+                    System.out.println(sayi1+"<"+sayi3+"<"+sayi2);
                 else
-                    System.out.println(sayi3+"<"+sayi2+"<"+sayi1);
+                {
+                    if(sayi1<sayi2)
+                        System.out.println(sayi3+"<"+sayi1+"<"+sayi2);
+                    else
+                        System.out.println(sayi3+"<"+sayi2+"<"+sayi1);
+                }
+
             }
 
+
         }
+        else
+            System.out.println(sayi1+"="+sayi2+"="+sayi3);
 
 
 
